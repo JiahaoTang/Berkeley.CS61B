ee29fb008ed0e714e34323a47967b694553274a0
JiahaoTang <jiahaotang1994@gmail.com>
2018-02-14 14:37:30 -0500

    Finished GraphDB in proj3

--- project/project3/proj3/src/main/java/GraphDB.java
+++ project/project3/proj3/src/main/java/GraphDB.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.util.ArrayList;
+import java.util.*;
+import java.lang.Long;
 
 /**
  * Graph for storing all of the intersection (vertex) and road (edge) information.
@@ -17,9 +18,53 @@ import java.util.ArrayList;
  * @author Alan Yao, Josh Hug
  */
 public class GraphDB {
+    private final Map<Long, Node> nodes = new HashMap<>();
+    private final Map<Long, Way> ways = new HashMap<>();
+    private final Map<Long, Relation> relations = new HashMap<>();
+
     /** Your instance variables for storing the graph. You should consider
      * creating helper classes, e.g. Node, Edge, etc. */
+    /*Node class is used for storing nodes in the map.*/
+    static class Node{
+        Long id;
+        double lat;
+        double lon;
+        Map<Long, String> extraInfo;
 
+        Node(Long id, double lon, double lat){
+            this.id = id;
+            this.lat = lat;
+            this.lon = lon;
+            extraInfo = new HashMap<>();
+        }
+    }
+    /*Way class is used for storing ways in the map.*/
+    static class Way{
+        Long id;
+        Set<Node> nodeSet;
+        Map<String, String> extraInfo;
+
+        Way(Long id){
+            this.id = id;
+            nodeSet = new HashSet<>();
+            extraInfo = new HashMap<>();
+        }
+    }
+
+    /*Relation class is used for storint relationships in the map.*/
+    static class Relation{
+        Long id;
+        Map<Node, String> nodeMembers;
+        Map<Way, String> wayMembers;
+        Map<String, String> extraInfo;
+
+        Relation(Long id){
+            this.id = id;
+            nodeMembers = new HashMap<>();
+            wayMembers = new HashMap<>();
+            extraInfo = new HashMap<>();
+        }
+    }
     /**
      * Example constructor shows how to create and start an XML parser.
      * You do not need to modify this constructor, but you're welcome to do so.
@@ -54,12 +99,32 @@ public class GraphDB {
      */
     private void clean() {
         // TODO: Your code here.
+        Set<Node> connectedNodes = new HashSet<>();
+        for(Long wayId:ways.keySet()){
+            for(Node node:ways.get(wayId).nodeSet){
+                connectedNodes.add(node);
+            }
+        }
+        for(Long relationId:relations.keySet()){
+            for(Node node:relations.get(relationId).nodeMembers.keySet()){
+                connectedNodes.add(node);
+            }
+        }
+        for(Long nodeId: nodes.keySet()){
+            if(!connectedNodes.contains(nodes.get(nodeId))){
+                nodes.remove(nodeId);
+            }
+        }
     }
 
     /** Returns an iterable of all vertex IDs in the graph. */
     Iterable<Long> vertices() {
-        //YOUR CODE HERE, this currently returns only an empty list.
-        return new ArrayList<Long>();
+        //TODO: YOUR CODE HERE, this currently returns only an empty list.
+        ArrayList<Long> res = new ArrayList<>();
+        for(Long vId : nodes.keySet()){
+            res.add(vId);
+        }
+        return res;
     }
 
     /** Returns ids of all vertices adjacent to v. */
@@ -70,21 +135,32 @@ public class GraphDB {
     /** Returns the Euclidean distance between vertices v and w, where Euclidean distance
      *  is defined as sqrt( (lonV - lonV)^2 + (latV - latV)^2 ). */
     double distance(long v, long w) {
-        return 0;
+        Node nodeV = nodes.get(v);
+        Node nodeW = nodes.get(w);
+        return Math.sqrt(Math.pow(nodeV.lat - nodeW.lat, 2) + Math.pow(nodeV.lon - nodeW.lon, 2));
     }
 
     /** Returns the vertex id closest to the given longitude and latitude. */
     long closest(double lon, double lat) {
-        return 0;
+        double minDis = Double.MAX_VALUE;
+        long res = 1L;
+        for(Long nodesId : nodes.keySet()){
+            double dis = Math.sqrt(Math.pow(lat - nodes.get(nodesId).lat, 2) + Math.pow(lon - nodes.get(nodesId).lon, 2));
+            if(dis < minDis){
+                minDis = dis;
+                res = nodesId;
+            }
+        }
+        return res;
     }
 
     /** Longitude of vertex v. */
     double lon(long v) {
-        return 0;
+        return nodes.get(v).lon;
     }
 
     /** Latitude of vertex v. */
     double lat(long v) {
-        return 0;
+        return nodes.get(v).lat;
     }
 }
